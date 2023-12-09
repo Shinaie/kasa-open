3d3000a84f1db034965696ef935af4154f56ac78
James Brossard <shinaie@hotmail.fr>
2023-12-09 11:34:22 +0100

    composents react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Apropos from "./pages/Apropos";
+import Logement from "./pages/Logement";
+import Error from "./pages/Error";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/a-propos" element={<Apropos />} />
+        <Route path="/logement" element={<Logement />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
