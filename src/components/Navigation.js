f9d769c15927fc00fb898975fddfb7fdcd5a1878
James Brossard <shinaie@hotmail.fr>
2023-12-09 10:32:18 +0100

    premier commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../img/LOGO.png";
+
+const Navigation = () => {
+  return (
+    <div>
+      <img src={logo} className="logo" alt="logo"></img>
+    </div>
+  );
+};
+
+export default Navigation;
