e594f0679d2921fc2924db57d96458570f214c72
James Brossard <shinaie@hotmail.fr>
2023-12-13 17:45:01 +0100

    navlink

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,59 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Card = () => {
   return (
     <div className="gallery">
-      <div className="card">
-        <p>Titre de la location</p>
-      </div>
-      <div className="card">
-        <p>Titre de la location</p>
-      </div>
-      <div className="card">
-        <p>Titre de la location</p>
-      </div>
-      <div className="card">
-        <p>Titre de la location</p>
-      </div>
-      <div className="card">
-        <p>Titre de la location</p>
-      </div>
-      <div className="card">
-        <p>Titre de la location</p>
-      </div>
+      <NavLink to="./logement">
+        <div className="card">
+          <p>Titre de la location</p>
+        </div>
+      </NavLink>
+      <NavLink to="./logement">
+        <div className="card">
+          <p>Titre de la location</p>
+        </div>
+      </NavLink>
+      <NavLink to="./logement">
+        <div className="card">
+          <p>Titre de la location</p>
+        </div>
+      </NavLink>
+      <NavLink to="./logement">
+        <div className="card">
+          <p>Titre de la location</p>
+        </div>
+      </NavLink>
+      <NavLink to="./logement">
+        <div className="card">
+          <p>Titre de la location</p>
+        </div>
+      </NavLink>
+      <NavLink to="./logement">
+        <div className="card">
+          <p>Titre de la location</p>
+        </div>
+      </NavLink>
+      <NavLink to="./logement">
+        <div className="card">
+          <p>Titre de la location</p>
+        </div>
+      </NavLink>
+      <NavLink to="./logement">
+        <div className="card">
+          <p>Titre de la location</p>
+        </div>
+      </NavLink>
+      <NavLink to="./logement">
+        <div className="card">
+          <p>Titre de la location</p>
+        </div>
+      </NavLink>
+      <NavLink to="./logement">
+        <div className="card">
+          <p>Titre de la location</p>
+        </div>
+      </NavLink>
     </div>
   );
 };
